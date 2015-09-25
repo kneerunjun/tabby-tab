9f1e82468f7d78b87006b12c9c1a8bb5d4b764c2
kneerunjun <kneerunjun@outlook.com>
2015-09-25 13:27:50 +0530

    committing the leftover files

---
+++ tabby-tab-website/ng/controllers/mainController.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    var mainController = angular.module("myApp").controller("mainController", function ($scope, $window) {
+        $scope.gotoWikiPage = function () {
+            $window.location.href = "https://github.com/kneerunjun/tabby-tab/wiki";
+        }
+    })
+})();
